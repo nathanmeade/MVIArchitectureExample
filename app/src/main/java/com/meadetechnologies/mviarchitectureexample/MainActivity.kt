fe5fd80ef46ea0f5b9add5354a270569156104f2
Nathan Meade <nathan.r.meade@gmail.com>
2020-06-09 10:13:40 -0400

    feat: MainFragment implemented

--- app/src/main/java/com/meadetechnologies/mviarchitectureexample/MainActivity.kt
+++ app/src/main/java/com/meadetechnologies/mviarchitectureexample/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showMainFragment()
+    }
+
+    fun showMainFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MainFragment(), "MainFragment")
+            .commit()
     }
 }
---
+++ app/src/main/java/com/meadetechnologies/mviarchitectureexample/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.meadetechnologies.mviarchitectureexample
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
